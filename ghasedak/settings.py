19aaa94be31cafa64e2e3da86f22cb99e4623413
mohsenksr <mohsen.kassiri@live.com>
2023-08-01 23:49:33 +0330

    Set allowed hosts

--- ghasedak/settings.py
+++ ghasedak/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3sw(y@(vxq6ux+k-ulug-b!=8c_(b6e$$dyf2-f21htxg*=%+7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
