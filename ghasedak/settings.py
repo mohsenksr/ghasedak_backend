8684d1adea17321e75129b0f19d2cec12e0a03dc
mohsenksr <mohsen.kassiri@live.com>
2023-08-18 20:18:43 +0330

    Add trusted csrf

--- ghasedak/settings.py
+++ ghasedak/settings.py
@@ -52,10 +52,14 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://ghasedak.darkube.app'
 ]
 
 ROOT_URLCONF = 'ghasedak.urls'
